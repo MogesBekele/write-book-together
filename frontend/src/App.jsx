464d15d53a457bebcf6f97d69f6b4668811655fd
MogesBekele <mogesbekele32@gmail.com>
2025-04-22 14:31:16 +0300

    add toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import Register from "./component/Register";
 import Login from "./component/Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./component/Home";
 import Book from "./component/dashboard/Book";
 import Collaborate from "./component/dashboard/Collaborate";
 import Profile from "./component/dashboard/Profile";
 import Dashboard from "./component/dashboard/Dashboard";
 import Settings from "./component/dashboard/Settings";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer position="top-right" autoClose={1500} />
 
       <Router>
         <Routes>
           {/* Public Routes */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/home" />} /> {/* Redirect to /home */}
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
           {/* Dashboard Routes */}
           <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<Navigate to="books" />} /> {/* Redirect to /dashboard/books */}
             <Route path="books" element={<Book />} />
             <Route path="collaborate" element={<Collaborate />} />
             <Route path="profile" element={<Profile />} />
