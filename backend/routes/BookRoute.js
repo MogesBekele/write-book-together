70a729e67cb5f3ad8663d1010ed06304c792e0be
MogesBekele <mogesbekele32@gmail.com>
2025-04-23 11:13:32 +0300

    edit on book delete and edit section

--- backend/routes/BookRoute.js
+++ backend/routes/BookRoute.js
@@ -3,11 +3,15 @@ import verifyToken from "../middleware/VerifyToken.js";
 import {
   contributeToBook,
   getBookContent,
+  deleteContribution,
+  editContribution,
 } from "../controller/BookController.js";
 
 const router = express.Router();
 
 router.post("/contribute", verifyToken, contributeToBook);
 router.get("/", getBookContent);
+router.delete("/contribution/:id", verifyToken, deleteContribution);
+router.put("/contribution/:id", verifyToken, editContribution);
 
 export default router;
