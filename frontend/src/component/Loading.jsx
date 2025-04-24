e18c14715db0a426f5b26f21599cdf7a21bc8fd3
MogesBekele <mogesbekele32@gmail.com>
2025-04-24 14:03:23 +0300

    animate the loading div

--- frontend/src/component/Loading.jsx
+++ frontend/src/component/Loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
+      <div className="w-24 h-24 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
     </div>
   );
 };
