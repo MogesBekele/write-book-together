9a2a94172eb2ca87911ad8e0e1efe9e88faef175
MogesBekele <mogesbekele32@gmail.com>
2025-04-29 14:03:12 +0300

    commit on app context

--- frontend/src/component/context/Context.jsx
+++ frontend/src/component/context/Context.jsx
@@ -2,12 +2,21 @@ import { createContext } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const [book, setBook] = useState(null); // State to store book details
-  const [loading, setLoading] = useState(true); // State to track loading
-  const [error, setError] = useState(null); // State to handle errors
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const value ={
+        title,
+        setTitle,
+        description,
+        setDescription,
+
+    }
+
+
 
   return (
-    <AppContext.Provider value={{ book, setBook, loading, setLoading, error, setError }}>
+    <AppContext.Provider value={{value}}>
       {children}
     </AppContext.Provider>
   );
