b15fe6db7083ebf5fd4d2fe0a17b7b0340ee7c99
MogesBekele <mogesbekele32@gmail.com>
2025-04-29 14:00:37 +0300

    create app context

--- frontend/src/component/context/Context.jsx
+++ frontend/src/component/context/Context.jsx
@@ -1,2 +1,14 @@
 import { createContext } from "react";
 const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [book, setBook] = useState(null); // State to store book details
+  const [loading, setLoading] = useState(true); // State to track loading
+  const [error, setError] = useState(null); // State to handle errors
+
+  return (
+    <AppContext.Provider value={{ book, setBook, loading, setLoading, error, setError }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
