57b98262ea09a7c1ee9fe605be13e3fb84091164
MogesBekele <mogesbekele32@gmail.com>
2025-05-01 13:38:55 +0300

    remove the bug

--- frontend/src/component/dashboard/GetAllBook.jsx
+++ frontend/src/component/dashboard/GetAllBook.jsx
@@ -13,7 +13,7 @@ const GetAllBook = () => {
     setExpandedBooks,
     loading,
     setLoading,
-    token, // Get the token from context
+    token
   } = useContext(AppContext).value;
   const toastShown = useRef(false); // Ref to track if toast has been shown
 
