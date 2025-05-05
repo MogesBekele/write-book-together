7ada040c292e6f74e8828858b4cb8a882db1d276
MogesBekele <mogesbekele32@gmail.com>
2025-05-05 16:36:10 +0300

    commit on the bookroute

--- backend/routes/BookRoute.js
+++ backend/routes/BookRoute.js
@@ -16,20 +16,12 @@ const router = express.Router();
 router.post("/add", VerifyToken, addBook); // Add a new book
 router.get("/", VerifyToken, getAllBooks); // Get all books
 router.get("/:bookId", VerifyToken, getBookById); // Get a book by ID
-
 // Contribution Routes
 router.post("/:bookId/contributions", VerifyToken, addContribution); // Add a contribution to a book
 router.get("/:bookId/contributions", VerifyToken, getContributions); // Get contributions for a book
 // bookRoutes.js or similar
-router.put(
-  "/:bookId/contributions/:contributionId",
-  VerifyToken,
-  editContribution
-);
-router.delete(
-  "/:bookId/contributions/:contributionId",
-  VerifyToken,
-  deleteContribution
-);
+router.put('/:bookId/contributions/:contributionId', VerifyToken, editContribution);
+router.delete('/:bookId/contributions/:contributionId', VerifyToken, deleteContribution);
+
 
 export default router;
